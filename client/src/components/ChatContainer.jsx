17e44087f8ba0c9ae81fa12fb7afb8a64a6fe26f
HarishNandhaKumar <harishkumar2171@gmail.com>
2025-07-02 23:04:32 +0530

    fix: Changed Utils to utils.js

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useRef, useState } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
-import { formatMessageTime } from '../lib/Utils'
+import { formatMessageTime } from '../lib/utils.js'
 import { ChatContext } from '../../context/ChatContext'
 import { AuthContext } from '../../context/AuthContext'
 
